fb3af30f44ee7a5308ac42d7d09b8eb87fa6545f
Lumgol <88503543+Lumgol@users.noreply.github.com>
2024-11-25 19:33:48 -0500

    character_completed

--- PlayMode.hpp
+++ PlayMode.hpp
@@ -173,6 +173,8 @@ struct PlayMode : Mode {
 		uint8_t leaving_line = 0; // 0 = false, 1 = true, 2 = waiting to leave line until join animation finishes
 		int8_t asset_idx = -1;
 
+		bool character_completed = false;
+
 		std::string entrance_file = "";
 		std::string puzzle_file = "";
 		uint entrance_line = 0;
